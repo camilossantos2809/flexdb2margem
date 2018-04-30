4f6b0eeb223c4b1386e77c90086b4e89db6e9f91
Camilo Santos <camilossantos.2809@gmail.com>
2018-04-30 14:28:29 -0300

    Concluída SQL 2

---
+++ 02_ticket_medio_dia.sql
@@ -0,0 +1,17 @@
+-- Ticket médio
+SELECT
+    cast(avg(
+        case 
+            when vdo_norm_canc='N'
+            then vdo_valor else vdo_valor * (-1)
+        end
+    ) as numeric(10,2)) as valor
+FROM
+    vdonline
+where 
+    (
+        vdo_data >= CURRENT_DATE 
+        and vdo_data < CURRENT_DATE + interval '1 day'
+    )
+    and vdo_unidade = '001'
+    and vdo_tipo in('V','v');
