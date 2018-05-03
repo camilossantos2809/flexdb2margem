0d5c364ef8069c7ea5e1258f3045cd8969282344
Camilo Santos <camilossantos.2809@gmail.com>
2018-05-03 11:57:45 -0300

    Criação de função para retornar dados da query 11

--- 00_schema.sql
+++ 00_schema.sql
@@ -7,6 +7,7 @@ update vdonline set vdo_data = current_date + cast(vdo_data as time);
 create extension if not exists postgres_fdw;
 
 create schema if not exists erp;
+create schema if not exists margem;
 
 CREATE SERVER erp 
     FOREIGN DATA WRAPPER postgres_fdw 
@@ -19,9 +20,74 @@ CREATE SERVER erp
 create user mapping for postgres 
     server erp options(user 'postgres', password 'rp1064');
 
-import foreign schema public limit to(
-    departamentos,
+import foreign schema public 
+limit to(
     vdonlineprod
 )
 from server erp
 into erp;
+
+create or replace function margem.fn_itens_desconto(
+    data_mvto date,
+    unidade integer
+)
+returns table (
+    dataproc date,
+    hora time,
+    operador text,
+    supervisor text,
+    valor numeric(15,3),
+    nro_cupom varchar(6),
+    item integer,
+    codigo_ean varchar(13),
+    desc_item text
+)
+language plpgsql strict as $$
+    declare
+        v_tabela text;
+        v_unidade text;
+        v_query text;
+    begin
+        v_tabela := format('tab_venda_%s', to_char(data_mvto, 'MMyy'));
+        v_unidade := quote_literal(lpad(unidade::text, 3, '0'));
+        v_query := format($sql$
+            with vendas as (
+                select
+                    tvd_unidade,
+                    tvd_data_hora,
+                    tvd_pdv,
+                    tvd_cupom,
+                    tvd_operador,
+                    tvd_cpseq,
+                    string_to_array(tvd_registro, '|') as tvd_registro
+                from %s
+                where
+                    (
+                        tvd_data_hora >= $1 
+                        and tvd_data_hora < $1 + interval '1 day'
+                    )
+                    and tvd_tipo_reg = 'VITN'
+                    and tvd_unidade = %s
+            )
+            select
+                cast(tvd_data_hora as date) as dataproc,
+                cast(tvd_data_hora as time) as hora,
+                op.usu_nome::text as operador,
+                sup.usu_nome::text as supervisor,
+                round(cast(tvd_registro[29] as numeric)/100,2) as valor,
+                tvd_cupom as nro_cupom,
+                cast(tvd_cpseq as integer)-1 as item,
+                tvd_registro[1]::varchar(13) as codigo_ean,
+                tvd_registro[2]::text as desc_item
+            from vendas
+                left join usuario op
+                    on (tvd_operador=op.usu_codigo)
+                left join usuario sup
+                    on (tvd_registro[22]=sup.usu_codigo)
+            where tvd_registro[29]<>''
+        $sql$, v_tabela, v_unidade);
+
+        return query execute v_query using(data_mvto);
+        return;
+    end
+$$;
