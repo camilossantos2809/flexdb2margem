124126b760bcd1018d99ef9bc00a78a74f288d58
Camilo Santos <camilossantos.2809@gmail.com>
2018-08-08 15:43:52 -0300

    Corrigido retorno de custos na query 1 agrupando tuplas por produto e totalizando no final

--- 01_total_venda_valor.sql
+++ 01_total_venda_valor.sql
@@ -3,10 +3,12 @@
 */
 
 with prun as (
-    select prun_prod_codigo, prun_ctmedio, prun_ctvenda
+    select
+        prun_prod_codigo,
+        prun_ctmedio,
+        prun_ctvenda
     from erp.produn
     where prun_unid_codigo='001' --lpad(cast(numeroLoja as varchar),3,'0')
-        and prun_ativo = 'S'
         and (
             prun_ctmedio > 0
             or prun_ctvenda > 0
@@ -14,19 +16,19 @@ with prun as (
 ), total_venda as (
     SELECT
         cast(sum(coalesce(vopr_valor - vopr_desconto, 0)) as numeric(10,2)) as valor,
-        cast(sum(coalesce(prun_ctmedio,0) + coalesce(prun_ctvenda,0)) as numeric(10,2)) as custo
+        cast((max(coalesce(prun_ctmedio,0)) + max(coalesce(prun_ctvenda,0)))*sum(vopr_qtde) as numeric(10,2)) as custo
     FROM
         erp.vdonlineprod
             left join prun
-                on (
-                    vopr_prod_codigo=prun_prod_codigo
-                )
+                on (vopr_prod_codigo=prun_prod_codigo)
     where 
         vopr_datamvto=CURRENT_DATE
         and vopr_unid_codigo = '001'--lpad(cast(numeroLoja as varchar),3,'0')
+    group by
+        vopr_prod_codigo
 )
 select
-    valor,
-    custo,
-    cast(valor - custo as numeric(10,2)) as lucro
+    cast(sum(valor) as numeric(10,2)) as valor,
+    cast(sum(custo) as numeric(10,2)) as custo,
+    cast(sum(valor) - sum(custo) as numeric(10,2)) as lucro
 from total_venda;
