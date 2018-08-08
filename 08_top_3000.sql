c3dd096c60e032e45b0ff1dc83640b1b5d6815b3
Camilo Santos <camilossantos.2809@gmail.com>
2018-08-08 15:46:30 -0300

    Implementado retorno de informações de custo e lucro nas queries 7 e 8

--- 08_top_3000.sql
+++ 08_top_3000.sql
@@ -1,23 +1,53 @@
 -- top 3000 produtos por valor
+
+with prun as (
+    select
+        prun_prod_codigo, 
+        prun_ctmedio,
+        prun_ctvenda
+    from erp.produn
+    where prun_unid_codigo='001' --lpad(cast(numeroLoja as varchar),3,'0')
+        and prun_ativo = 'S'
+        and (
+            prun_ctmedio > 0
+            or prun_ctvenda > 0
+        )
+), top3000 as(
+    select
+        ite_codbarra as codigo,
+        ite_descricao as descricao,
+        max(coalesce(tpc_preco,0)) as valor_unitario,
+        sum(coalesce(vopr_qtde,0)) as quantidade,
+        sum(coalesce(vopr_valor-vopr_desconto,0)) as valor,
+        cast(coalesce(prun_ctmedio,0) + coalesce(prun_ctvenda,0) as numeric(10,2)) as custo
+    from
+        erp.vdonlineprod vd
+            left join tabitens
+                on (vd.vopr_prod_codigo=cast(ite_cod_interno as numeric))
+            left join tabprecos2
+                on (vd.vopr_prod_codigo=cast(tpc_cod_interno as numeric))
+            left join prun
+                on (vopr_prod_codigo=prun_prod_codigo)
+    where
+        vd.vopr_datamvto=CURRENT_DATE
+        and vd.vopr_unid_codigo = '001'--lpad(cast(numeroLoja as varchar),3,'0')
+        and tpc_unidade = '001'--lpad(cast(numeroLoja as varchar),3,'0')
+    group by
+        ite_codbarra,
+        ite_descricao,
+        prun_ctmedio,
+        prun_ctvenda
+    order by
+        "valor" desc
+    limit 3000
+)
 select
-    ite_codbarra as codigo,
-    ite_descricao as descricao,
-    max(coalesce(tpc_preco,0)) as valor_unitario,
-    sum(coalesce(vopr_qtde,0)) as quantidade,
-    sum(coalesce(vopr_valor-vopr_desconto,0)) as valor
+    codigo,
+    descricao,
+    valor_unitario,
+    quantidade,
+    valor,
+    cast(custo*quantidade as numeric(10,2)) as custo,
+    cast(valor-(custo*quantidade) as numeric (10,2)) as lucro
 from
-    erp.vdonlineprod vd
-        left join tabitens
-            on (vd.vopr_prod_codigo=cast(ite_cod_interno as numeric))
-        left join tabprecos2
-            on (vd.vopr_prod_codigo=cast(tpc_cod_interno as numeric))
-where 
-    vd.vopr_datamvto=CURRENT_DATE
-    and vd.vopr_unid_codigo = '001'--lpad(cast(numeroLoja as varchar),3,'0')
-    and tpc_unidade = '001'--lpad(cast(numeroLoja as varchar),3,'0')
-group by
-    ite_codbarra,
-    ite_descricao
-order by
-    "valor" desc
-limit 3000;
+    top3000;
