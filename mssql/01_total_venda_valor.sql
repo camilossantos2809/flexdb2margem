5cac112438107b6b24bd3ed566f2967ef9abd9d2
Camilo <camilossantos.2809@gmail.com>
2018-09-05 10:19:22 -0300

    Corrigida a query mssql 1 para retornar dados de lucratividade

--- mssql/01_total_venda_valor.sql
+++ mssql/01_total_venda_valor.sql
@@ -1,15 +1,34 @@
--- Total vendido do dia 
-SELECT
-    cast(sum(
-        case 
-            when vdo_norm_canc='N'
-            then vdo_valor else vdo_valor * (-1)
-        end
-    ) as numeric(10,2)) as valor
-FROM
-    vdonline
-where 
-    cast(vdo_data as date) = cast(CURRENT_TIMESTAMP as date)
-    and vdo_unidade = '001' --right('000'+cast(numeroLoja as varchar(3)), 3)
-    and vdo_norm_canc = 'N'
-go
+/*
+    Retorna o total vendido do dia
+*/
+
+with prun as (
+    select
+        prun_prod_codigo,
+        prun_ctmedio,
+        prun_ctvenda
+    from erp.dbo.produn
+    where prun_unid_codigo='001' --right('000'+cast(numeroLoja as varchar(3)), 3)
+        and (
+            prun_ctmedio > 0
+            or prun_ctvenda > 0
+        )
+), total_venda as (
+    SELECT
+        cast(sum(coalesce(vopr_valor - vopr_desconto, 0)) as numeric(10,2)) as valor,
+        cast((max(coalesce(prun_ctmedio,0)) + max(coalesce(prun_ctvenda,0)))*sum(vopr_qtde) as numeric(10,2)) as custo
+    FROM
+        erp.dbo.vdonlineprod
+            left join prun
+                on (vopr_prod_codigo=prun_prod_codigo)
+    where 
+        vopr_datamvto=cast(CURRENT_TIMESTAMP as date)
+        and vopr_unid_codigo = '001'--right('000'+cast(numeroLoja as varchar(3)), 3)
+    group by
+        vopr_prod_codigo
+)
+select
+    cast(sum(valor) as numeric(10,2)) as valor,
+    cast(sum(custo) as numeric(10,2)) as custo,
+    cast(sum(valor) - sum(custo) as numeric(10,2)) as lucro
+from total_venda;
