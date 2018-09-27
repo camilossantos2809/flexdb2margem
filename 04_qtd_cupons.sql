5f4afd83f6f57045e3fba33b91c29be2941de6dd
Camilo Santos <camilossantos.2809@gmail.com>
2018-09-27 17:01:24 -0300

    Correção da consulta 4

--- 04_qtd_cupons.sql
+++ 04_qtd_cupons.sql
@@ -1,12 +1,16 @@
 -- Quantidade cupons válidos
+
 SELECT
-    cast(count(distinct vdo_cupom) as integer) as qtd_cupons
+    cast(count(distinct (vdo_cupom,vdo_pdv)) filter (where vdo_norm_canc='N') as integer) 
+    - cast(count(distinct (vdo_cupom,vdo_pdv)) filter (where vdo_norm_canc='C') as integer) 
+    as qtd_cupons
 FROM
     vdonline
 where 
     (
         vdo_data >= CURRENT_DATE 
-        and vdo_data < CURRENT_DATE + interval '1 day'
+        and vdo_data < CURRENT_DATE::date + interval '1 day'
     )
     and vdo_unidade = '001'--lpad(cast(numeroLoja as varchar),3,'0')
-    and vdo_tipo in('V');
+    and vdo_tipo in('V','v','O','o')
+    and vdo_final<>'';
