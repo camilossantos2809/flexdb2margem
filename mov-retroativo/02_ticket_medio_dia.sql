b29e08c23dd9be1f5891d1365c5f4fdb44e27c77
Camilo Santos <camilossantos.2809@gmail.com>
2018-10-01 10:28:38 -0300

    Mov Retroativo: Adicionada query que retorna média dos valores vendidos por cupom

---
+++ mov-retroativo/02_ticket_medio_dia.sql
@@ -0,0 +1,13 @@
+-- Ticket médio
+with cupons as (
+    SELECT
+        cast(sum(coalesce(vopr_valor-vopr_desconto,0)) as numeric(10,2)) as valor
+    FROM
+        vdonlineprod
+    where 
+        vopr_datamvto = '2018-09-27'
+        and vopr_unid_codigo = '001'--lpad(cast(numeroLoja as varchar),3,'0')
+    group by vopr_cupom, vopr_pdvs_codigo
+)
+select round(avg(valor),2)
+from cupons;
