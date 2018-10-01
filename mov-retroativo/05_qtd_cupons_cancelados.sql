2be28832b1e48ac0757b3a34efbccd4d4bbb02b3
Camilo Santos <camilossantos.2809@gmail.com>
2018-10-01 14:02:41 -0300

    Mov retroativo: Adicionado código para retorno de quantidade de cupons

---
+++ mov-retroativo/05_qtd_cupons_cancelados.sql
@@ -0,0 +1,8 @@
+-- Quantidade cupons válidos
+SELECT
+    count(*) as qtd_cupons
+from vdonlineprod
+where
+    vopr_datamvto='2018-09-27'
+    and vopr_unid_codigo = '001' --lpad(cast(1 as varchar),3,'0')
+    and vopr_valor < 0;
