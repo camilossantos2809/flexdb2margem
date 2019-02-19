be522b2b447b4945cd55cdca9511e29bd0097e82
Camilo Santos <camilossantos.2809@gmail.com>
2019-02-19 16:06:01 -0300

    Comandos para permitir que o usuário gestorrp consiga acessar tabelas do erp

--- 00_1_schema_erp.sql
+++ 00_1_schema_erp.sql
@@ -1,2 +1,5 @@
 -- Comandos a ser executados no database erp
+grant select on table movprodc to gestorrp;
+grant select on table movfechpdv to gestorrp;
+grant select on table produn to gestorrp;
 grant select on table vdonlineprod to gestorrp;
