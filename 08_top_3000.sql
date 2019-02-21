a103e65d3e87c6f56d20d7a974247387eae51f93
Camilo <camilossantos.2809@gmail.com>
2019-02-21 09:42:49 -0300

    Otimização da query 07 e 08 (top 100 e top 3000)

--- 08_top_3000.sql
+++ 08_top_3000.sql
@@ -1,23 +1,36 @@
 -- top 3000 produtos por valor
+with vendas as (
+    select
+        vopr_prod_codigo,
+        sum(coalesce(vopr_qtde,0)) as quantidade,
+        sum(coalesce(vopr_valor-vopr_desconto,0)) as valor
+    from
+        erp.vdonlineprod vd
+    where
+        vd.vopr_datamvto=CURRENT_DATE
+        and vd.vopr_unid_codigo = lpad(cast(numeroLoja as varchar),3,'0')
+    group by
+        vopr_prod_codigo
+    order by
+        "valor" desc
+    limit 3000
+)
 select
     ite_codbarra as codigo,
     ite_descricao as descricao,
-    max(coalesce(tpc_preco,0)) as valor_unitario,
-    sum(coalesce(vopr_qtde,0)) as quantidade,
-    sum(coalesce(vopr_valor-vopr_desconto,0)) as valor
+    tpc_preco as valor_unitario,
+    quantidade,
+    valor
 from
-    erp.vdonlineprod vd
+    vendas vd
         left join tabitens
             on (vd.vopr_prod_codigo=cast(ite_cod_interno as numeric))
         left join tabprecos2
-            on (vd.vopr_prod_codigo=cast(tpc_cod_interno as numeric))
-where 
-    vd.vopr_datamvto=CURRENT_DATE
-    and vd.vopr_unid_codigo = '001'--lpad(cast(numeroLoja as varchar),3,'0')
-    and tpc_unidade = '001'--lpad(cast(numeroLoja as varchar),3,'0')
-group by
-    ite_codbarra,
-    ite_descricao
+            on (
+                vd.vopr_prod_codigo=cast(tpc_cod_interno as numeric)
+                and ite_cod_interno = tpc_cod_interno
+                )
+where
+    tpc_unidade = lpad(cast(numeroLoja as varchar),3,'0')
 order by
-    "valor" desc
-limit 3000;
+    valor desc;
