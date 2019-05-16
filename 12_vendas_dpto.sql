e957938c286469ee38f3c840b8cc5681983e898c
Camilo <camilossantos.2809@gmail.com>
2019-05-16 10:04:03 -0300

    otimização de query 12_vendas_dpto.sql

--- 12_vendas_dpto.sql
+++ 12_vendas_dpto.sql
@@ -1,4 +1,14 @@
 -- vendas por departamento/seção
+
+with prod_dptos as (
+    select 
+        cast(ite_cod_interno as numeric) as prod_codigo,
+        dpt_codigo,
+        dpt_nome
+    from tabitens
+        inner join departamentos
+            on (ite_cod_dpto=dpt_codigo)
+)
 select
     dpt_codigo as codigo,
     dpt_nome as descricao,
@@ -6,13 +16,11 @@ select
     sum(coalesce(vopr_valor-vopr_desconto,0)) as valor
 from
     erp.vdonlineprod vd
-        left join tabitens
-            on (vd.vopr_prod_codigo=cast(ite_cod_interno as numeric))
-        left join departamentos
-            on (ite_cod_dpto=dpt_codigo)
+        left join prod_dptos
+            on (vd.vopr_prod_codigo=prod_codigo)
 where
     vd.vopr_datamvto = CURRENT_DATE
-    and vd.vopr_unid_codigo = '001'--lpad(cast(numeroLoja as varchar),3,'0')
+    and vd.vopr_unid_codigo = lpad(cast(numeroLoja as varchar),3,'0')
 group by
     dpt_codigo,
     dpt_nome
